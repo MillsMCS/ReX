b2a4227ea936a5c32f65a45de9c81c28a29c094f
Karena <khuang@mills.edu>
2019-04-24 13:15:10 -0700

    Database pull request needed

--- app/src/main/java/com/cs115/rex/RexDatabaseHelper.java
+++ app/src/main/java/com/cs115/rex/RexDatabaseHelper.java
@@ -27,15 +27,15 @@ public class RexDatabaseHelper extends SQLiteOpenHelper {
 
 
     private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (oldVersion < 1) {
-            db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + "NAME TEXT, "
-                    + "DESCRIPTION TEXT, "
-                    + "IMAGE_RESOURCE_ID INTEGER);");
-            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
-            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
-                    R.drawable.cappuccino);
-            insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+//        if (oldVersion < 1) {
+//            db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+//                    + "NAME TEXT, "
+//                    + "DESCRIPTION TEXT, "
+//                    + "IMAGE_RESOURCE_ID INTEGER);");
+//            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+//            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
+//                    R.drawable.cappuccino);
+//            insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
 
         }
 }
