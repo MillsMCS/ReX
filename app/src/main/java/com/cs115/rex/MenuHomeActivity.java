9486f2527e0d9865162232dc9c86487362dc0da2
Zoe Abrams <zoej.abrams@gmail.com>
2019-05-02 15:11:34 -0700

    Added dog house home button for portrait orientation

---
+++ app/src/main/java/com/cs115/rex/MenuHomeActivity.java
@@ -0,0 +1,12 @@
+package com.cs115.rex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
