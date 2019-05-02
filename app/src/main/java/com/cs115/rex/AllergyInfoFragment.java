16bf9ab21442eb354895767465b815bdf3a04241
Maygan <mnlightstone@gmail.com>
2019-05-01 21:49:29 -0700

    skeleton of Allergy functionality added.
    StringArray is currently hardcoded; needs to be changed to pull from database.
    On Spinner item select, a Toast is shown. This needs to be changed to adding a button to the fragment's layout.
    Delete functionality is completely untouched.

--- app/src/main/java/com/cs115/rex/AllergyInfoFragment.java
+++ app/src/main/java/com/cs115/rex/AllergyInfoFragment.java
@@ -6,17 +6,52 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 /**
  *
  */
-public class AllergyInfoFragment extends Fragment {
+public class AllergyInfoFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+    String[] arraySpinner;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_allergy_info, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        arraySpinner = new String[] {
+                "1", "2", "3", "4", "5", "6", "7"
+        };
+
+        Spinner s = getActivity().findViewById(R.id.all_foods_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                                                        getActivity(),
+                                                        android.R.layout.simple_spinner_item,
+                                                        arraySpinner);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
+
+        s.setOnItemSelectedListener(this);
+    }
+
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        // TODO Auto-generated method stub
+        Toast.makeText(getActivity(), arraySpinner[position], Toast.LENGTH_SHORT).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 
 }
