d2f5fb20629bf933cab0637c87f94838e7990ebb
Maygan <mnlightstone@gmail.com>
2019-05-01 21:06:12 -0700

    changes to DogInfoFragment are reflected in database

--- app/src/main/java/com/cs115/rex/DogInfoFragment.java
+++ app/src/main/java/com/cs115/rex/DogInfoFragment.java
@@ -20,9 +20,15 @@ public class DogInfoFragment extends Fragment {
     private EditText nameET;
     private EditText weightET;
     private EditText breedET;
+
+    private String oldName;
+    private String oldBreed;
+    private String oldWeight;
+
     private String name;
     private String breed;
     private String weight;
+
     private boolean isRestored;
     private boolean isEditing;
 
@@ -36,6 +42,9 @@ public class DogInfoFragment extends Fragment {
             weight = savedInstanceState.getString("weight");
             isRestored = savedInstanceState.getBoolean("isRestored");
             isEditing = savedInstanceState.getBoolean("isEditing");
+            oldName = savedInstanceState.getString("oldName");
+            oldBreed = savedInstanceState.getString("oldBreed");
+            oldWeight = savedInstanceState.getString("oldWeight");
         }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dog_info, container, false);
@@ -45,58 +54,86 @@ public class DogInfoFragment extends Fragment {
     public void onStart() {
         super.onStart();
         View view = getView();
+
+        // set up EditText Views
         nameET = view.findViewById(R.id.nameView);
         nameET.setEnabled(isEditing);
-
         breedET = view.findViewById(R.id.breedView);
         breedET.setEnabled(isEditing);
-
         weightET = view.findViewById(R.id.WeightView);
         weightET.setEnabled(isEditing);
 
+        // if we have restored a previous state, put in String values
         if (isRestored) {
             nameET.setText(name);
             weightET.setText(weight);
             breedET.setText(breed);
+
+        // if this is the first time loading the Activity, load values from database
         } else {
             Cursor cursor = RexDatabaseUtilities.getDog(view.getContext());
             if (cursor.moveToFirst()) {
-                nameET.setText(cursor.getString(0));
-                weightET.setText(cursor.getString(1));
-                breedET.setText(cursor.getString(2));
+                // put values in String variables so we can close cursor
+                oldName = cursor.getString(0);
+                oldWeight = cursor.getString(1);
+                oldBreed = cursor.getString(2);
+
+                // set Edit Texts to values
+                nameET.setText(oldName);
+                weightET.setText(oldWeight);
+                breedET.setText(oldBreed);
+
+                cursor.close();
             }
         }
     }
 
     /**
      * Called by ProfileActivity on Edit / Save button press.
+     * //TODO write more Javadoc
      */
     public void changeEditableStatus(){
+
+        // if user is Editing, compare their new values to their old values
+        if (isEditing){
+            String newName = nameET.getText().toString();
+            String newBreed = breedET.getText().toString();
+            String newWeight = weightET.getText().toString();
+
+            if (!oldName.equals(newName)) {
+                RexDatabaseUtilities.updateName(getActivity().getApplicationContext(), newName);
+            }
+            if (!oldBreed.equals(newBreed)){
+                RexDatabaseUtilities.updateBreed(getActivity().getApplicationContext(), newBreed);
+            }
+            if (!oldWeight.equals(newWeight)){
+                RexDatabaseUtilities.updateWeight(getActivity().getApplicationContext(), newWeight);
+            }
+        }
+
         isEditing = !isEditing;
         nameET.setEnabled(isEditing);
         breedET.setEnabled(isEditing);
         weightET.setEnabled(isEditing);
     }
 
-
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        // save original values
+        savedInstanceState.putString("oldName", oldName);
+        savedInstanceState.putString("oldBreed", oldBreed);
+        savedInstanceState.putString("oldWeight", oldWeight);
+
+        // save current values
         savedInstanceState.putString("name", nameET.getText().toString());
         savedInstanceState.putString("breed", breedET.getText().toString());
         savedInstanceState.putString("weight", weightET.getText().toString());
+
+        // save booleans
         savedInstanceState.putBoolean("isRestored", true);
         savedInstanceState.putBoolean("isEditing", true);
     }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
-    // get cursor of dog from database
-    // load editTextView values from cursor
-    // on save:
-    // compare all editTextViews to cursor values
-    // run appropriate methods (where values != )
 }
 
 
