8edbfd3518114888850e4f83f719d130bacac9a6
Zoe Abrams <zoej.abrams@gmail.com>
2019-05-02 15:40:04 -0700

    finishing up adding dog house home button

--- app/src/main/java/com/cs115/rex/MenuHomeActivity.java
+++ app/src/main/java/com/cs115/rex/MenuHomeActivity.java
@@ -1,12 +1,38 @@
 package com.cs115.rex;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MenuHomeActivity extends AppCompatActivity {
+    // DetailActivity and ResultsActivity extend this activity so that they display a home button
+    // on phones and on tablets in portrait orientation
 
+    //Menu - adds and activates dog house home button and paw button from menu_with_home.xml
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu to add items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_with_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Menu - activates menu paw button to open profile interface
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                Intent mIntent = new Intent(this, MainActivity.class);
+                startActivity(mIntent);
+                return true;
+
+            case R.id.action_profile:
+                Intent pIntent = new Intent(this, ProfileActivity.class);
+                startActivity(pIntent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
